01187b0b34b903a6090795ca6d3932314d8ee02c
yancai <yancai915@gmail.com>
2020-06-25 11:30:15 +0800

    完善cpu, memory已使用值的查询

--- demo.py
+++ demo.py
@@ -2,40 +2,67 @@
 # -*- coding:utf-8 -*-
 import json
 from datetime import datetime
-from typing import Any
 
 from kubernetes import config, client
 from kubernetes.client import CoreV1Api
-from kubernetes.client import ApiClient
-
 
 from settings import KUBE_CONFIG
 
-"""
-id
-batch
-cluster
-node
-namespace
-pod
-container
-cpu_request
-cpu_limit
-cpu_used
-mem_request
-mem_limit
-mem_used
-
-"""
 
 _client = None
+_container_metrics_map = dict()
+
+
+def format_cpu(cpu):
+    """规整cpu单位为n
+    1n = 10^-9
+    1u = 10^-6
+    1m = 10^-3
+
+    :return:
+    """
+
+    if cpu is None:
+        return cpu
+
+    if cpu.endswith("n"):
+        return cpu
+    if cpu.endswith("m"):
+        value = int(cpu[0:-1]) * 1000
+        return str(value) + "n"
+    if cpu.isdigit():
+        value = int(cpu) * 1000 * 1000
+        return str(value) + "n"
+
+
+def format_mem(mem):
+    """规整memory单位为Ki
+    1 Mi = 1024 Ki
+    1 Gi = 1024 Mi
+
+    :return:
+    """
+
+    if mem is None:
+        return mem
+
+    if mem.endswith("Ki"):
+        return mem
+    if mem.endswith("Mi"):
+        value = int(mem[0:-2]) * 1024
+        return str(value) + "Ki"
+    if mem.endswith("Gi"):
+        value = int(mem[0:-2]) * 1024 * 1024
+        return str(value) + "Gi"
 
 
 class ContainerResource(object):
-    def __init__(self, cluster="", node=None, namespace="", pod="", container="",
-                 cpu_request="", cpu_limit="", cpu_used="",
-                 mem_request="", mem_limit="", mem_used="",
-                 sample_time=""):
+    def __init__(self,
+                 cluster=None, node=None, namespace=None, pod=None,
+                 container=None,
+                 cpu_request=None, cpu_limit=None, cpu_used=None,
+                 mem_request=None, mem_limit=None, mem_used=None,
+                 sample_time=None):
         super().__init__()
         self.cluster = cluster
         self.node = node
@@ -57,12 +84,12 @@ class ContainerResource(object):
             "namespace": self.namespace,
             "pod": self.pod,
             "container": self.container,
-            "cpu_request": self.cpu_request,
-            "cpu_limit": self.cpu_limit,
-            "cpu_used": self.cpu_used,
-            "mem_request": self.mem_request,
-            "mem_limit": self.mem_limit,
-            "mem_used": self.mem_used,
+            "cpu_request": format_cpu(self.cpu_request),
+            "cpu_limit": format_cpu(self.cpu_limit),
+            "cpu_used": format_cpu(self.cpu_used),
+            "mem_request": format_mem(self.mem_request),
+            "mem_limit": format_mem(self.mem_limit),
+            "mem_used": format_mem(self.mem_used),
             "sample_time": self.sample_time,
         }
 
@@ -90,7 +117,15 @@ def list_container_res_by_pod(pod_data):
             con_res.mem_limit = c.resources.limits.get("memory", None)
             con_res.cpu_limit = c.resources.limits.get("cpu", None)
 
-        con_reses.append(con_res)
+        con_metric_map = get_container_metrics_by_namespaced(con_res.namespace)
+        key = "-".join([con_res.namespace, con_res.pod, con_res.container])
+        con_res.cpu_used = con_metric_map.get(key, {}).get("cpu", None)
+        con_res.mem_used = con_metric_map.get(key, {}).get("memory", None)
+
+        con_res.sample_time = con_metric_map.get(key, {}).get("timestamp", None)
+
+        if con_res.cpu_used is not None:
+            con_reses.append(con_res)
 
     return con_reses
 
@@ -133,6 +168,39 @@ def get_k8s_client():
     return _client
 
 
+def get_container_metrics_by_namespaced(ns):
+    """获取某一namespace下的所有容器资源已使用值
+    [
+      {
+        "cpu": "1166876n",
+        "memory": "9716Ki",
+        "timestamp": "2020-06-25T03:07:06Z",
+      }
+    ]
+
+    :param ns:
+    :return:
+    """
+    global _container_metrics_map
+    if ns not in _container_metrics_map:
+        api = client.CustomObjectsApi()
+        data = api.list_namespaced_custom_object(
+            "metrics.k8s.io", "v1beta1", ns, "pods")
+
+        cmap = dict()
+
+        for pod in data["items"]:
+            pod_name = pod["metadata"]["name"]
+            for container in pod["containers"]:
+                key = "-".join([ns, pod_name, container["name"]])
+                cmap[key] = container["usage"]
+                cmap[key]["timestamp"] = pod["timestamp"]
+
+        _container_metrics_map[ns] = cmap
+
+    return _container_metrics_map[ns]
+
+
 def main_fun():
     init()
     k8scli = get_k8s_client()
@@ -147,38 +215,12 @@ def main_fun():
     print(json.dumps(result))
 
 
-def main():
-    config.load_kube_config(KUBE_CONFIG)
-
-    cli = client.CoreV1Api()
-    # nodes = cli.list_node()
-    # ips = [i.metadata.name for i in nodes.items]
-    # for i in ips:
-    #     print(i)
-
-    pods = cli.list_pod_for_all_namespaces(limit=1)
-    print(pods)
-    # pod_status = cli.read_namespaced_pod_status(namespace="ingress-nginx", name="default-http-backend-67cf578fc4-7ddj7", pretty=True)
-    # print(pod_status)
-
-
-    # api = client.CustomObjectsApi()
-    # # metric = api.get_namespaced_custom_object(
-    # #     "metrics.k8s.io", "v1beta1", "ingress-nginx", "pods",
-    # #     "default-http-backend-67cf578fc4-7ddj7k"
-    # # )
-    # metric = api.list_cluster_custom_object("metrics.k8s.io", "v1beta1", "pods")
-    #
-    # js = json.dumps(metric)
-
-    # print(js)
-    # print(metric)
-
-
-    pass
-
-
 if __name__ == "__main__":
+    start = datetime.now()
     main_fun()
-    # main()
+    end = datetime.now()
+
+    print("start: {}\nend: {}\nused: {}".format(
+        start, end, end - start
+    ))
     pass
